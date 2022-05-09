8ec3bc08e005df947f21ed55afb77591841b8737
Kian Metcalfe <kian@metcalfe.net>
2022-05-09 16:28:50 -0400

    Updated script

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo Uploading build files...
-rsync -q -avzhP --no-p ./build/* root@10.0.0.10:/mnt/user/www
-echo Client build uploaded.
+echo -e '\033[0;34m'Uploading build files...'\033[0m'
+rsync -avzh --no-p ./build/* root@10.0.0.10:/mnt/user/www
+echo -e '\033[0;36m'Client build uploaded.'\033[0m'
 
-echo Your portfolio has been updated!
+echo -e '\033[0;32m'Your portfolio has been updated!'\033[0m'
